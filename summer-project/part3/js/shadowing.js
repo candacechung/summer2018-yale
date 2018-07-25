dd78b790c2f5c4e0d3050437400ddc94735801d5
Chung Nian Chyun <chungnianchyun@Chungs-Air-2.wireless.yale.internal>
2018-07-25 14:12:16 -0400

    bookmarklet test

---
+++ summer-project/part3/js/shadowing.js
@@ -0,0 +1,39 @@
+
+(function(){
+// This part checks for jQuery
+var version = "1.10.2";
+// Checks for prior inclusion and version
+if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
+        // If there isn't an instance of jQuery, create one and append it to the head
+        // Else run our bookmarklet!
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "https://ajax.googleapis.com/ajax/libs/jquery/" + version + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function(){
+                if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                        done = true;
+                        initBookmarklet(window.jQuery);
+                }
+    else {
+        console.log('error loading');
+    }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+if (window.jQuery === undefined || window.jQuery.fn.jquery < version) {
+    console.log(':::');
+}
+else {
+    console.log('jquery loaded');
+}
+} else {
+        initBookmarklet(window.jQuery);
+}
+function initBookmarklet($) {
+        (window.bookmarklet = function() {
+                // Replace this section with your own Information Overlay script
+                $("img").css("-webkit-box-shadow: 0px 10px 30px 6px rgba(0,0,0,0.75)"
+                "-moz-box-shadow: 0px 10px 30px 6px rgba(0,0,0,0.75)"
+                "box-shadow: 0px 10px 30px 6px rgba(0,0,0,0.75);");""
+        })();
+}
+})();
